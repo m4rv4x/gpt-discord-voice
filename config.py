bdd68d2c8e2a28c11f3fbebc4b3abc78ab6c6a9c
marvax <33978801+m4rv4x@users.noreply.github.com>
2023-06-05 16:44:45 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+### OPEN AI CHAT GPT CONFIG
+openai.api_key = "OPENAI API Key"
+openai.role = "Persona of AI"
+openai.model = "gpt-3.5-turbo"
+
+### TEXT TO VOICE API
+elevenlabs_api_key = "Elevenlabs API Key"
+
+### DISCORD BOT API
+discord_api_token = 'Your Discord Bot Token'
+discord_target_channel_id = your_voice_channel_num
+
+### OPTIONS
+tts_provider = "elevenlabs"
+
+### USAGE 
+prefix = "!"
+name = "gpt"
+
